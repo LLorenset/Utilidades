4c40a47c75b1e1b0e3acb5283becf2d1400503ae
LLorenset <lorenzo.com45@gmail.com>
2026-03-10 09:58:25 +0100

    prueba de commit

--- laboratorio/clase.py
+++ laboratorio/clase.py
@@ -1,6 +1,7 @@
 
 class A: pass
 class B: pass
+
 class C(A,B): pass
 
 print(C.mro())
--- laboratorio/clase2.py
+++ laboratorio/clase2.py
@@ -1,6 +1,7 @@
 class Carrito(object):
     def __new__(cls, *args, **kwargs):
         print('__new__')
+        
         return super().__new__(cls)
 
     def __init__(self):
